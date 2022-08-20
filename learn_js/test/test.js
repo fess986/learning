d783b359a72833122c02fb6bfd6560e4bffdf578
Fess986 <fess986@gmail.com>
2022-08-20 15:51:46 +0300

    adding some react-learning files

--- learn_js/test/test.js
+++ learn_js/test/test.js
@@ -1,9 +1,22 @@
-window.addEventListener('unhandledrejection', function(event) {
-    // объект события имеет два специальных свойства:
-    alert(event.promise); // [object Promise] - промис, который сгенерировал ошибку
-    alert(event.reason); // Error: Ошибка! - объект ошибки, которая не была обработана
-  });
+// window.addEventListener('unhandledrejection', function(event) {
+//     // объект события имеет два специальных свойства:
+//     alert(event.promise); // [object Promise] - промис, который сгенерировал ошибку
+//     alert(event.reason); // Error: Ошибка! - объект ошибки, которая не была обработана
+//   });
   
-  new Promise(function() {
-    throw new Error("Ошибка!");
-  }); // нет обработчика ошибок
+//   new Promise(function() {
+//     throw new Error("Ошибка!");
+//   }); // нет обработчика ошибок
+
+const apple = {
+  ass: 'big',
+  [Symbol.toStringTag]: 'very',
+}
+
+console.log(apple);
+
+console.log(Object.prototype.toString.call(apple))
+console.log(parseInt('42px')); // 42
+console.log(parseInt('42px42')); // 42 - идет до первой ошибки
+console.log(parseInt(+'42px')); // NaN
+console.log(Object.getOwnPropertyDescriptors([].__proto__))
