e6d1677680bc3193609078c7ba3a330f60a3b05a
fess986u <fess986@mail.ru>
2024-11-08 17:30:43 +0300

    add svelte

--- node.js/MERN - node mongo react express/app.js
+++ node.js/MERN - node mongo react express/app.js
@@ -2,14 +2,29 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth.routes');
+const { path } = require ('path')
 
 const app = express();
 const PORT = config.get('port') || 5000;
 
-app.use(express.json({ extended: true })) // изначально в бэке всё прилетает в виде стримов, для того чтобы вместо этого получать обычный JSON-объект - добавляем данный mw
+app.use(express.json({ extended: true })) // изначально в бэке всё прилетает в виде стримов, для того чтобы вместо этого получать обычный JSON-объект - добавляем данный mw - обязадельно ДО рабочих запросов
 
 app.use('/api/auth', authRoutes)  // создаём запрос, который обязательно должен начинаться с /api - и используем в нём роут router из модуля auth.routes, который был экспортирован по умолчанию, так что можно не создавать отдельную переменную под него.
 app.use('/api/link', require('./routes/link.routes')) 
+
+// mw который будет отдавать по корневым запросам статику - должен находиться в конце запросов mw
+
+if (process.env.NODE_ENV === 'production') {
+  // Это встроенное middleware Express.js, которое обслуживает статические файлы, такие как HTML, CSS, JavaScript и изображения
+app.use('/', express.static(path.join(__dirname, 'client', 'build'))); 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});  // файл который будет высылаться при любом запросе
+// Эта строчка кода указывает Express.js отправлять файл `index.html` для всех GET-запросов, которые не были обработаны другими маршрутами. Это часто используется в одностраничных приложениях (SPA), чтобы обеспечить поддержку маршрутизации на стороне клиента.
+}
+
+
 
 const start = async function() {
   try {
---
+++ svelte/svelte-app/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let name = 'SvelteKit1';
+  let src = 'https://avatars.githubusercontent.com/u/1162160?v=4'
+  const altText = 'Rich Harris'
+  let htmlText = '<b>Strong</b> and <i>Emphasized</i> text'
+
+  console.log('ass')
+</script>
+
+<style>
+  h1 {
+    color: red;
+  }
+
+  img {
+    width: 100px;
+    height: auto;
+  }
+</style>
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img {src} alt={altText}>
+
+<!-- если хотим вставить html в js то используем этот способ с @html-->
+<p>{@html htmlText}</p>  
